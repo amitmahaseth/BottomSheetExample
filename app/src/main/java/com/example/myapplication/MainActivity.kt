02b33ec40cd624953b951580adb67576c93a83ea
amitmahaseth <amitmahaseth6@gmail.com>
2022-01-26 14:02:48 +0530

    26/01/2022

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,22 +1,20 @@
 package com.example.myapplication
 
-import android.app.Dialog
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.view.ViewGroup
 import android.view.Window
 import android.widget.TextView
 import android.widget.Toast
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnDialog.setOnClickListener {
             showDialog()
@@ -25,18 +23,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showDialog() {
-        val dialog=Dialog(this)
+        val dialog=BottomSheetDialog(this,R.style.SheetDialog)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setContentView(R.layout.bottom_sheet_layout)
 
         val textLayout=dialog.findViewById<TextView>(R.id.tv1)
-        textLayout.setOnClickListener {
+        textLayout?.setOnClickListener {
             Toast.makeText(this,"waw!",Toast.LENGTH_SHORT).show()
         }
         dialog.show()
-        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT)
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        dialog.window?.attributes?.windowAnimations=R.style.DialogAnimation
-        dialog.window?.setGravity(Gravity.BOTTOM)
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT)
+
     }
 }
